377f2e791c68579f1bda8cf9a1052af838ec5f77
Brian Guzmán <b.guzman.m@me.com>
2023-04-06 22:32:39 -0400

    clon de instagram

---
+++ js/function.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+  $("a#img5").fancybox({
+    'transitionIn': 'elastic',
+    'transitionOut': 'elastic',
+    'speedIn': 600,
+    'speedOut': 200,
+    'overlayShow': false
+  });
+  $("a#img6").fancybox({
+    'transitionIn': 'elastic',
+    'transitionOut': 'elastic',
+    'speedIn': 600,
+    'speedOut': 200,
+    'overlayShow': false
+  });
+  $("a#img7").fancybox({
+    'transitionIn': 'elastic',
+    'transitionOut': 'elastic',
+    'speedIn': 600,
+    'speedOut': 200,
+    'overlayShow': false
+  });
+});
