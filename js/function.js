f423f8d32304f32c5a2c020e4212dca687d98689
Brian Guzmán <b.guzman.m@me.com>
2023-04-10 21:09:06 -0400

    agrego comentarios

--- js/function.js
+++ js/function.js
@@ -1,4 +1,23 @@
+function changeColor(element) {
+  $(element).toggleClass('text-danger');
+}
+
+function visibilityOfComments(element) {
+  var identificador = $(element).attr('id').split('-')[1];
+  $('#comments-' + identificador).toggleClass('visually-hidden');
+}
+
+function addComment(element) {
+  var identificador = $(element).attr('id').split('-')[1];
+  var elMensaje = $(`#msg-${identificador}`).val();
+  $(`#listOfComments-${identificador} ul`).append(`<li>${elMensaje}</li>`);
+}
+
 $(document).ready(() => {
+
+  $('.fa-regular').click(function () { changeColor(this) });
+  $('.fa-comment').click(function () { visibilityOfComments(this) });
+  $('.btn-primary').click(function () { addComment(this) });
   $("a#img5").fancybox({
     'transitionIn': 'elastic',
     'transitionOut': 'elastic',
@@ -21,3 +40,4 @@ $(document).ready(() => {
     'overlayShow': false
   });
 });
+
